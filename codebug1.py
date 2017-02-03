e8b63bd2049572ac41dd1ff457a489dc89ae94a5
Shmurs <torti894@gmail.com>
2017-02-03 12:54:04 +0100

    Ayyyyy

---
+++ codebug1.py
@@ -0,0 +1,55 @@
+# Codebug1
+# By A Torti
+# 2nd Feb 2017
+
+
+for index in range(99,0, -1):
+    #thename = input("What is your name?")
+    #print(thename)
+    line = ""
+    for length in range(0,index):
+        line = line + "Annachiara"
+    print(line)
+
+for index in range (0,index) :
+    # thename = input("What is your name?")
+    # print(thename)
+    line = ""
+    for length in range(0, index):
+        line = line + "Ian"
+    print(line)
+
+    for index in range(99, 0, -1):
+        # thename = input("What is your name?")
+        # print(thename)
+        line = ""
+        for length in range(0, index):
+            line = line + "Olivia"
+        print(line)
+
+    for index in range(0, index):
+        # thename = input("What is your name?")
+        # print(thename)
+        line = ""
+        for length in range(0, index):
+            line = line + "Guia"
+        print(line)
+
+        for index in range(99, 0, -1):
+            # thename = input("What is your name?")
+            # print(thename)
+            line = ""
+            for length in range(0, index):
+                line = line + "Benjamin"
+            print(line)
+
+        for index in range(0, index):
+            # thename = input("What is your name?")
+            # print(thename)
+            line = ""
+            for length in range(0, index):
+                line = line + "Matteo"
+            print(line)
+
+            
+
